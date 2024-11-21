0553ad4f2b5958b6b5b2f08daeb3ac2896cf6116
Mohamed Taib <mohataibmt23003@gmail.com>
2024-11-21 22:00:21 +0100

    tri_tas

--- tp2.py
+++ tp2.py
@@ -2,7 +2,7 @@ def swap(T, i, j):
     T[i], T[j] = T[j], T[i]
 
 
-def tas(T, n, i):
+def entasser(T, n, i):
     big = i  
     left = (i * 2) + 1  
     right = (i * 2) + 2  
@@ -15,26 +15,37 @@ def tas(T, n, i):
 
     if big != i:
         swap(T, i, big)  
-        tas(T, n, big)  
+        entasser(T, n, big)  
 
 
-def tasmax(T):
+def construire_tas(T):
     n = len(T)
     for i in range(n // 2 - 1, -1, -1):
-        parent = (i - 1) // 2  # Compute the parent index
-
-        # Compare the current node with its parent during the heapify process
+        parent = (i - 1) // 2  
         if parent >= 0 and T[parent] < T[i]:  
             swap(T, parent, i)
+        entasser(T, n, i)
 
-        # Heapify the subtree rooted at the current node
-        tas(T, n, i)
+def tri_tas (r) :
+      construire_tas(r)
+      n = len(r)
+      t=[]
 
+      for i in range(n - 1, 0, -1):
+        r[0], r[i] = r[i], r[0]
+        t.append(r.pop())  
+        entasser(r, len(r), 0)
+
+      t.append(r[0])
+      return t
 
 # Example array
 T = [10, 22, 5, 18, 20, 25, 40, 30, 35, 12]
 print("Original array:", T)
 
-tasmax(T)
+construire_tas(T)
 print("Max heap:", T)
+
+tri = tri_tas (T)
+print("TAS tri :", tri) 
 
